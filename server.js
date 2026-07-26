bc5827453a8cd02ba419e25d70630f42154aecfe
Musa-Stark <musa.fullstack08@gmail.com>
2026-07-26 16:07:00 +0500

    feat: print headers + trust proxy

--- server.js
+++ server.js
@@ -2,20 +2,14 @@ import express from "express";
 import "dotenv/config";
 
 const app = express();
+app.set("trust proxy", true);
 const PORT = process.env.PORT;
 
 app.get("/api/v1/health", (req, res) => {
+  console.log(req.headers)
   res.json({
     success: true,
     message: "All systems running on an optimal protocol.",
-  });
-});
-
-app.get("/api/v1/req", (req, res) => {
-  console.log(req)
-  res.json({
-    success: true,
-    message: "req printed in console",
   });
 });
 
